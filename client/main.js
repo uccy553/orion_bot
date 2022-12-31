2efecc730bc648beab0b44c6456dbf5f1cc630f9
uccy553 <77891127+uccy553@users.noreply.github.com>
2022-12-31 14:50:30 +0100

    Update main.js

--- client/main.js
+++ client/main.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server
 
-  const response = await fetch('http://localhost:5000/', {
+  const response = await fetch('https://orion-5siw.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
